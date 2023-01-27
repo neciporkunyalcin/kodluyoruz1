cff10a0a536e53062ba578a88cd570aaeaee5fe7
necip orkun yalçın <117844809+neciporkunyalcin@users.noreply.github.com>
2023-01-27 14:47:59 +0300

    Add files via upload

---
+++ Yeni klasör (3)/ödev1.js
@@ -0,0 +1,8 @@
+let name = prompt("isminizi giriniz");
+let textname;
+let myName = document.querySelector("#myName")
+let myClock = document.querySelector("#myClock")
+myName.innerHTML = (name)
+myClock.classList.add("clock")
+myClock.innerHTML = (myClock)
+
